c100a6cb208b0956a0c1e810f87eace78b79a15e
Adam Slack <adam.slack@bjss.com>
2019-05-05 15:01:26 +0100

    [unit tests] - fleshing out memorydb unit tests. removed empty placeholder suite

--- streamer/lib/streamer-db/tests/memorydb.test.js
+++ streamer/lib/streamer-db/tests/memorydb.test.js
@@ -1,6 +1,26 @@
 const MemoryDB = require('../providers/memorydb')
 
 let db
+
+
+const testUserId = 'USERID001'
+const testSubscriptionId = 'SUBID001'
+
+const testSubscription = {
+    [testSubscriptionId]: {
+        meta: {
+            name: 'Test subscription data'
+        },
+        registedOn: '2019-05-05T13:57:14.235Z'
+    }
+}
+
+const testUserSubscriptions = {
+    [testUserId]: {
+        ...testSubscription
+    }
+}
+
 describe('MemoryDB implementation Class', () => {
     beforeEach(() => {
         db = new MemoryDB()
@@ -15,10 +35,16 @@ describe('MemoryDB implementation Class', () => {
     })
 
     describe('getUsersSubscriptions', () => {
-        it('Should resolve to empty object if userId is not in object of user subsciptions', () => {
+        it('Should resolve to empty object if userId is not in object of user subscriptions', () => {
             const fakeUserId = '0000'
             const actual = db.getUsersSubscriptions(fakeUserId)
             expect(actual).resolves.toBeFalsy()
+        })
+
+        it ('Should resolve to be an object of subscriptions if user has subscriptions', () => {
+            db.userSubscriptions = testUserSubscriptions
+            const actual = db.getUsersSubscriptions(testUserId)
+            expect(actual).resolves.toEqual(testSubscription)
         })
     })
 })
